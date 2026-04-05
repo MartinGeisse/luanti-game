8211d3a5b428d7b933617ed445409937e7d68ad8
Martin Geisse <martingeisse@googlemail.com>
2026-04-05 19:44:19 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 cd ~/.var/app/org.luanti.luanti/.minetest/games
 rm -rf my-cool-game
 cp -R ~/git-repos/luanti-game/my-cool-game .
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_1.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_2.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_3.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_4.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_5.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_6.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_7.ogg
+cp -R ~/git-repos/luanti-game/nonfree/mcl_jukebox_track_1.ogg ./my-cool-game/mods/mcl_jukebox/sounds/mcl_jukebox_track_8.ogg
